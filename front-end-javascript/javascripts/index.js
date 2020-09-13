c2fc85e6c13e151737320fb885a7b65d1356288f
Luca <lucakanyo@protonmail.com>
2020-09-13 11:53:30 -0400

    updated index.js with the working code from the practice file

--- front-end-javascript/javascripts/index.js
+++ front-end-javascript/javascripts/index.js
@@ -7,8 +7,8 @@ let notes = () => document.querySelector("#notes")
 
 API.loadBranches()
 
-noteForm().addEventListener("submit", addNote)
-notes().addEventListener("click", fetchNotes)
+noteForm().addEventListener("submit", API.addNote)
+notes().addEventListener("click", API.fetchNotes)
 
 function clearNotes(){
     noteCont().innerHTML = "" 
@@ -17,51 +17,4 @@ function clearNotes(){
 function clearForm(){
     document.querySelector("#note-content").value = ""
     document.querySelector("#philosopher-dropdown").value = "Select Philosopher"
-}
-
-
-function addNote(e){
-    e.preventDefault()
-    fetch("http://localhost:3000/notes", {
-    method: "POST",
-    headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-    },
-    body: JSON.stringify({
-        note: {
-            content: document.querySelector("#note-content").value,
-            philosopher_id: document.querySelector("#philosopher-dropdown").value
-        }
-    })
-    })
-    .then(resp => resp.json())
-    .then(obj => {
-        let note = new Note(obj.content, obj.philosopher, obj.id)
-        note.display()
-        clearForm()
-    })
-}
-
-
-function fetchNotes(){
-    fetch("http://localhost:3000/notes")
-    .then(resp => resp.json())
-    .then(data => {
-        clearNotes()
-        Note.createNotes(data)
-        Note.displayNotes(data)
-})
-}
-
-
-
-function deleteNote(e){
-    fetch("http://localhost:3000/notes/" + this.id, {
-        method: "delete"
-    })
-    .then(resp => resp.json())
-    .then(data => {
-        this.parentNode.remove();
-    })
 }
