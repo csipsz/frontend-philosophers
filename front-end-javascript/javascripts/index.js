eda2127560c6205923dcc82c11a43c6a53a6e76b
Luca <lucakanyo@protonmail.com>
2020-09-14 11:08:13 -0400

    redesigning html page

--- front-end-javascript/javascripts/index.js
+++ front-end-javascript/javascripts/index.js
@@ -3,6 +3,7 @@ let dropDown = () => document.querySelector("#philosopher-dropdown")
 let noteForm = () => document.querySelector('#note-submit')
 let noteCont = () => document.querySelector("#note-container")
 let notes = () => document.querySelector("#notes")
+document.querySelector("#note-content").value = "ADD YOUR COMMENT"
 
 API.loadBranches()
 
